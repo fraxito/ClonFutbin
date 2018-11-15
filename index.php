2476eb3da7b92bab61818cd2dcbafd8c9a6aa31d
Jorge Cisneros <fraxito@gmail.com>
2018-11-15 18:06:10 +0100

    Con los excels de la BBDD de jugadores

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+las imagenes estan aqui: https://fifa17.content.easports.com/fifa/fltOnlineAssets/CC8267B6-0817-4842-BB6A-A20F88B05418/2017/fut/items/images/players/html5/120x120/190043.png
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
